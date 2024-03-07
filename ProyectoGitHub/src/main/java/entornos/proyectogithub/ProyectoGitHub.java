da150328e68ede9750243ce68567c004237be4b6
Ismael <69457543+Astivero-x3d@users.noreply.github.com>
2024-03-07 16:32:18 +0100

    Update ProyectoGitHub.java

--- ProyectoGitHub/src/main/java/entornos/proyectogithub/ProyectoGitHub.java
+++ ProyectoGitHub/src/main/java/entornos/proyectogithub/ProyectoGitHub.java
@@ -9,5 +9,6 @@ public class ProyectoGitHub {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Github");
+        System.out.println("Github");
     }
 }
