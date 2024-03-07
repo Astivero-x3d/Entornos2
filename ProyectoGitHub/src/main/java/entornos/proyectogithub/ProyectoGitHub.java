a4b457ca8e0c099e4358b496f110ca79c462f446
Astivero-x3d <ismaelast2005@gmail.com>
2024-03-07 16:30:52 +0100

    Probando proyecto

---
+++ ProyectoGitHub/src/main/java/entornos/proyectogithub/ProyectoGitHub.java
@@ -0,0 +1,13 @@
+package entornos.proyectogithub;
+
+/**
+ *
+ * @author Ismael_Ast
+ */
+public class ProyectoGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Github");
+    }
+}
